b933a5fdcf7423af9d1663c32425b71f9157fdfc
oait-h-m <oussamaaithmed1@gmail.com>
2026-01-27 16:35:07 +0100

    add weapom.hpp

--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -8,10 +8,10 @@ class Weapon
     private:
         std::string type;
     public:
-        Weapon(const std::string& type);
+        Weapon(std::string type);
         ~Weapon();
         const std::string&  getType();
-        void    setType(const std::string& type);
+        void    setType(std::string type);
 };
 
 #endif
