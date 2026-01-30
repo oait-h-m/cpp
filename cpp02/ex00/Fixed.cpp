8fdc5bf4a3b1fc772b93c7fe4efa5d4c0d41512b
oait-h-m <oussamaaithmed1@gmail.com>
2026-01-30 15:54:53 +0100

    add Fixed.cpp

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -9,3 +9,23 @@ Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
 }
+
+Fixed	&Fixed::operator=(Fixed const &Other)
+{
+	if (this != &Other)
+	{
+		this->fixedPoint = Other.getRawBits();
+	}
+	return *this;
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->fixedPoint = raw;
+}
+
+int	Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->fixedPoint;
+}
