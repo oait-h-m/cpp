3de5cf49e319e454a856f4fca4761dfc39aa7bad
oait-h-m <oussamaaithmed1@gmail.com>
2026-02-03 20:45:09 +0100

    finished

--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -16,30 +16,34 @@ public:
 	Fixed(const Fixed &other);
 	~Fixed();
 
-	Fixed &operator=(const Fixed &obj);
-	bool operator>(const Fixed &other) const;
-	bool operator<(const Fixed &other) const;
-	bool operator>=(const Fixed &other) const;
-	bool operator<=(const Fixed &other) const;
-	bool operator==(const Fixed &other) const;
-	bool operator!=(const Fixed &other) const;
-
-	Fixed operator+(const Fixed &other) const;
-	Fixed operator-(const Fixed &other) const;
-	Fixed operator*(const Fixed &other) const;
-	Fixed operator/(const Fixed &other) const;
-
-
-
-	Fixed &operator++(void);
-	Fixed &operator--(void);
-	Fixed operator++(int);
-	Fixed operator--(int);
-
-	int getRawBits(void) const;
-        void setRawBits(int const raw);
+	int	getRawBits(void) const;
+        void	setRawBits(int const raw);
 	float	toFloat(void) const;
 	int	toInt(void) const;
+
+	Fixed	&operator=(const Fixed &obj);
+	bool	operator>(const Fixed &other) const;
+	bool	operator<(const Fixed &other) const;
+	bool	operator>=(const Fixed &other) const;
+	bool	operator<=(const Fixed &other) const;
+	bool	operator==(const Fixed &other) const;
+	bool	operator!=(const Fixed &other) const;
+
+	Fixed	operator+(const Fixed &other) const;
+	Fixed	operator-(const Fixed &other) const;
+	Fixed	operator*(const Fixed &other) const;
+	Fixed	operator/(const Fixed &other) const;
+
+	Fixed	&operator++(void);
+	Fixed	&operator--(void);
+	Fixed	operator++(int);
+	Fixed	operator--(int);
+
+	static Fixed		&min(Fixed &n1, Fixed &n2);
+	static Fixed		&max(Fixed &n1, Fixed &n2);
+	static const Fixed	&min(const Fixed &n1, const Fixed &n2);
+	static const Fixed	&max(const Fixed &n1, const Fixed &n2);
+
 };
 
 std::ostream	&operator<<(std::ostream &out, const Fixed &fixed);
