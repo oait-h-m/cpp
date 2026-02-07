587c913077ef03cdce100ee3e3b5b5c00a2dac66
oait-h-m <oussamaaithmed1@gmail.com>
2026-02-07 21:33:20 +0100

    finished

---
+++ cpp03/ex01/ScavTrap.cpp
@@ -0,0 +1,60 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() : ClapTrap()
+{
+	std::cout << "ScavTrap default constructor called" << std::endl;
+	name = "Unnamed";
+	hitPoints = 100; 
+	energyPoints = 50;
+	attackDamage = 20;
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap destructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string input) : ClapTrap(input)
+{
+	std::cout << "ScavTrap constructor called" << std::endl;
+	name = input;
+	hitPoints = 100; 
+	energyPoints = 50;
+	attackDamage = 20;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other)
+{
+	std::cout << "ScavTrap Copy constructor called" << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &other)
+{
+	std::cout << "ScavTrap copy assignment operator called" << std::endl;
+	if (this != &other)
+		ClapTrap::operator=(other);
+	return *this;
+}
+
+void	ScavTrap::attack(const std::string& target)
+{
+	if (hitPoints == 0)
+	{
+		std::cout << "ScavTrap " << name << " is dead and cannot attack!" << std::endl;
+		return;
+	}
+	if (energyPoints > 0)
+	{
+		std::cout << "ScavTrap " << name << " attacks " << target 
+			<< ", causing " << attackDamage << " points of damage!" << std::endl;
+		energyPoints--;
+	}
+	else
+		std::cout << "ScavTrap " << name << " Can't attack because no energyPoints left" 
+			<< std::endl;
+}
+
+void	ScavTrap::guardGate()
+{
+    std::cout << "ScavTrap " << name << " is now in Gate keeper mode" << std::endl;
+}
