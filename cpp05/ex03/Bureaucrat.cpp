573530bc6d04dc0fdf85a98adadcded89b5576dc
oait-h-m <oussamaaithmed1@gmail.com>
2026-02-28 06:15:26 +0000

    add Bureaucrat.cpp

---
+++ cpp05/ex03/Bureaucrat.cpp
@@ -0,0 +1,87 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string &input, int grade) : name(input), grade(grade) 
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+        	throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : name(other.name), grade(other.grade) {}
+
+Bureaucrat  &Bureaucrat::operator=(const Bureaucrat &other)
+{
+	if (this != &other)
+		grade = other.grade;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+std::string    Bureaucrat::getName() const
+{
+	return name;
+}
+
+int Bureaucrat::getGrade() const
+{
+	return grade;
+}
+
+void    Bureaucrat::incrementGrade()
+{
+	if (grade <= 1)
+		throw GradeTooHighException();
+	--grade;
+}
+
+void    Bureaucrat::decrementGrade()
+{
+	if (grade >= 150)
+		throw GradeTooLowException();
+	++grade;
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &obj)
+{
+	out << obj.getName() << ", bureaucrat grade " << obj.getGrade();
+	return out;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Grade too high!";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "Grade too low!";
+}
+void	Bureaucrat::signForm(AForm &form)
+{
+	try
+	{
+		form.beSigned(*this);
+		 std::cout << getName() << " signed " << form.getName() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << getName() << " couldn't sign " << form.getName()
+                  << " because " << e.what() << std::endl;
+	}
+}
+
+void Bureaucrat::executeForm(const AForm &form) const
+{
+    try
+    {
+        form.execute(*this);
+        std::cout << getName() << " executed " << form.getName() << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << getName() << " couldn't execute " << form.getName()
+                  << " because " << e.what() << std::endl;
+    }
+}
