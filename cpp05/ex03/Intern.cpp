85528d1ce65a2090dc66531a5ba0a1c9f59021dd
oait-h-m <oussamaaithmed1@gmail.com>
2026-03-03 03:07:16 +0000

    finish

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -14,9 +14,44 @@ Intern	&Intern::operator=(const Intern &other)
 
 Intern::~Intern() {}
 
+static	AForm* createShrubbery(const std::string &target)
+{
+	return new ShrubberyCreationForm(target);
+}
+
+static	AForm* createRobotomy(const std::string &target)
+{
+	return new RobotomyRequestForm(target);
+}
+
+static	AForm* createPresidential(const std::string &target)
+{
+	return new PresidentialPardonForm(target);
+}
+
 AForm *Intern::makeForm(const std::string &name, const std::string &target)
 {
+    const std::string formNames[3] = {
+        "shrubbery creation",
+        "robotomy request",
+        "presidential pardon"
+    };
 
-	std::cout << "Intern cannot create " << name << " because it is unknown." << std::endl;
-	return NULL;
+    AForm* (*functions[3])(const std::string &) = {
+        &createShrubbery,
+        &createRobotomy,
+        &createPresidential
+    };
+
+    for (int i = 0; i < 3; ++i)
+    {
+        if (formNames[i] == name)
+        {
+            std::cout << "Intern creates " << name << std::endl;
+            return functions[i](target);
+        }
+    }
+
+    std::cout << "Intern cannot create " << name << " because it is unknown." << std::endl;
+    return NULL;
 }
