a660a84eec67d1ac75e98dc3a0da5caffedda064
Oussama Ait H'mad <oussamaaithmed1@gmail.com>
2026-04-12 15:28:11 +0100

    update Aform header

--- cpp05/ex02/AForm.hpp
+++ cpp05/ex02/AForm.hpp
@@ -11,21 +11,23 @@ private:
 	bool			isSigned;
 	const int		gradeToSign;
 	const int		gradeToExecute;
+
 protected:
     virtual void executeAction() const = 0;
+
 public:
 	AForm(const std::string &name, int gradeToSign, int gradeToExecute);
 	AForm(const AForm &other);
 	AForm &operator=(const AForm &other);
 	virtual	~AForm();
 
-	std::string	getName() const;
-	bool		getIsSigned() const;
-	int		getGradeToSign() const;
-	int		getGradeToExecute() const;
+	std::string		getName() const;
+	bool			getIsSigned() const;
+	int				getGradeToSign() const;
+	int				getGradeToExecute() const;
 
-	void		beSigned(const Bureaucrat &bureaucrat);
-	virtual void		execute(Bureaucrat const & executor) const;
+	void			beSigned(const Bureaucrat &bureaucrat);
+	virtual void	execute(Bureaucrat const & executor) const;
 
 	class GradeTooHighException : public std::exception
 	{
